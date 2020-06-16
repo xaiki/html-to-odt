9eb6165395526651b1eb39372d544b790328cab8
Niv Sardi <xaiki@evilgiggle.com>
2020-06-16 16:24:04 -0300

    make this hack work
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,46 +1,114 @@
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
+ h1, figure, p {
+	 text-align: center;
+	 margin: 0 auto;
+ }
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
+ h1 {
+	 font-size: 2.8em;
+	 text-transform: uppercase;
+	 font-weight: 700;
+	 margin: 0 0 0.5em 0;
+ }
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+ figure {
+	 margin: 0 0 1em 0;
+	 display:flex;
+	 flex-direction: column;
+ }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+ img {
+	 width: 100%;
+	 max-width: 400px;
+	 margin: 1em auto;
+ }
 
-	p {
-		margin: 1em auto;
-	}
+ p {
+	 margin: 1em auto;
+ }
 
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
+ @media (min-width: 480px) {
+	 h1 {
+		 font-size: 4em;
+	 }
+ }
 </style>
 
+<script>
+ import { onMount} from 'svelte';
+
+ let fetch;
+
+ onMount(() => {
+	 fetch = window.fetch
+ })
+
+ function bufferToHex (buffer) {
+     return [...new Uint8Array (buffer)]
+         .map (b => b.toString (16).padStart (2, "0"))
+         .join ("");
+ }
+
+ function download(filename, data) {
+	 const element = document.createElement('a');
+	 element.setAttribute('href', URL.createObjectURL(data));
+	 element.setAttribute('download', filename);
+
+	 element.style.display = 'none';
+	 document.body.appendChild(element);
+
+	 element.click();
+
+	 document.body.removeChild(element);
+ }
+
+ async function handleClick() {
+	 const req = await fetch('/odt/blah', {method: 'POST'})
+	 console.error('req', req)
+ }
+
+
+ async function readFile(stream, type="application/vnd.oasis.opendocument.text") {
+	 let res = [];
+	 let chunk = {done: false};
+
+	 const reader = await stream.getReader()
+	 do {
+		 chunk = await reader.read();
+		 if (chunk.value) res.push(chunk.value.buffer)
+
+	 } while (! chunk.done);
+
+	 console.error('res', res)
+	 return new Blob(res, {type});
+ }
+
+ async function handleInput() {
+	 for (let i in this.files) {
+		 const file = this.files[i]
+		 const buf = await file.arrayBuffer()
+		 const sha256 = await crypto.subtle.digest('SHA-256', buf)
+
+		 const res = await fetch(`/odt/${bufferToHex(sha256)}`, {method: 'POST', body: buf})
+		 const body = await readFile(res.body)
+
+		 download(`${file.name}.odt`, body)
+	 }
+	 console.error(this.files)
+ }
+</script>
+
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Desodiador de Yas</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Sube tu HTML que yo te lo ODTifico</h1>
 
 <figure>
+	<p>
+		subi tu archivo aca: 
+		<input type="file" on:change={handleInput}/>
+	</p>
 	<img alt='Success Kid' src='successkid.jpg'>
-	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
--- src/server.js
+++ src/server.js
@@ -1,17 +1,98 @@
+import fs from 'fs';
+
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
+import Docker from 'dockerode';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-polka() // You can also use Express
-	.use(
+const docker = Docker();
+
+const CONTAINER_NAME = 'renothing/libreoffice'
+
+try {
+	fs.statSync('./tmp')
+} catch {
+	fs.mkdirSync('./tmp')
+}
+
+const startPolka = () => {
+	const app = polka() // You can also use Express
+	
+	app.post('/odt/:sum', (req, res) => {
+		let data = '';
+		req.on('data', chunk => data += chunk)
+		req.on('end', async () => {
+			console.error(`processing: ${req.params.sum}`)
+
+			const dirname = `./tmp/${req.params.sum}`
+
+
+			try {
+				fs.statSync(dirname)
+				fs.statSync(`${dirname}/data.odt`)
+			} catch {
+				try {
+					fs.mkdirSync(dirname)
+				} catch {
+					// pass
+				}
+
+				fs.writeFileSync(`${dirname}/data`, data)
+				
+				const run = await docker.run(
+					CONTAINER_NAME,
+					[
+						'loffice',
+						'--headless',
+						'--convert-to', 'odt',
+						'--outdir', `/tmp/${req.params.sum}/`, `${dirname}/data`],
+					process.stdout,
+					{
+						"HostConfig": {
+							"Binds": [
+								`${process.cwd()}/tmp:/tmp`,
+						]},
+						"Mounts": [
+							{
+								"Type": "bind",
+								"Source": "./tmp",
+								"Destination": "/tmp",
+								"Mode": "",
+								"RW": true,
+								"Propagation": "rprivate"
+							}
+						]
+				})
+			}
+
+			const file = fs.readFileSync(`${dirname}/data.odt`)
+			res.end(file)
+		})
+		
+	})
+	
+	app.use(
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
 	)
-	.listen(PORT, err => {
-		if (err) console.log('error', err);
-	});
+	   .listen(PORT, err => {
+		   if (err) console.log('error', err);
+	   });	
+}
+
+docker.pull(CONTAINER_NAME, (err, stream) => docker.modem.followProgress(
+	stream,
+	(err, output) => {
+		console.error('finished', output);
+	},
+	(event) => console.error('progress', event)
+))
+
+
+startPolka();
+
